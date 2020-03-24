cea5f398135f61c3f3e71494c4a80c830a60f674
Benjamin Kyhn <benkyhn@hotmail.com>
2020-03-24 14:21:53 +0100

    Fixed exercise 32.12 to correctly use the fork/join framework

--- src/Exercise_32_12.java
+++ src/Exercise_32_12.java
@@ -1,82 +1,71 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.Stream;
 
 public class Exercise_32_12 {
     public static void main(String[] args) {
-        final int SIZE = 9000000;
-        double[] list1 = new double[SIZE];
-        double[] list2 = new double[SIZE];
+        // Create a list
+        final int N = 9000000;
+        double[] list = new double[N];
 
-        // Assign random values to list1 in parallel
         long startTime = System.currentTimeMillis();
-        parallelAssignValues(list1); // Invoke parallel merge sort
+        parallelAssignValues(list);
         long endTime = System.currentTimeMillis();
-        System.out.println("\nParallel time with " + Runtime.getRuntime().availableProcessors() +
-                " processors is " + (endTime - startTime) + " milliseconds");
+        System.out.println("The number of processors is " + Runtime.getRuntime().availableProcessors());
+        System.out.println("Parallel time is " + (endTime - startTime) + " milliseconds");
+        System.out.println("Index 500000 is " + list[500000]);
 
-        // Assign random values to list2 in sequence
+
         startTime = System.currentTimeMillis();
         Random r = new Random();
-        for (int i = 0; i < list1.length; i++) {
-            list2[i] = r.nextDouble();
+        for (int i = 0; i < list.length; i++) {
+            list[i] = r.nextDouble();
         }
         endTime = System.currentTimeMillis();
         System.out.println("\nSequential time is " + (endTime - startTime) + " milliseconds");
+        System.out.println("Index 500000 is " + list[500000]);
     }
 
     public static void parallelAssignValues(double[] list){
-        RecursiveAction mainTask = new AssignValuesTask(list);
+        RecursiveTask<double[]> mainTask = new AssignValuesTask(list, 0, list.length);
         ForkJoinPool pool = new ForkJoinPool();
         pool.invoke(mainTask);
-
     }
 
-    private static class AssignValuesTask extends RecursiveAction{
-        private final int THRESHOLD = 500;
+    private static class AssignValuesTask extends RecursiveTask<double[]>{
+        private final int THRESHOLD = 1000;
         private double[] list;
+        private int low;
+        private int high;
         private Random r = new Random();
 
-        AssignValuesTask(double[] list){
+        public AssignValuesTask(double[] list, int low, int high){
             this.list = list;
+            this.low = low;
+            this.high = high;
         }
 
         @Override
-        protected void compute() {
-            if (list.length < THRESHOLD)
-                for (int i = 0; i < list.length; i++) {
+        protected double[] compute() {
+            if (high - low < THRESHOLD){
+                for (int i = low; i < high; i++)
                     list[i] = r.nextDouble();
-                }
+                return list;
+            }
             else {
-                // Obtain the first half
-                double[] firstHalf = new double[list.length/2];
-                System.arraycopy(list, 0, firstHalf, 0, list.length/2);
+                int mid = (low + high) / 2;
+                RecursiveTask<double[]> left = new AssignValuesTask(list, low, mid);
+                RecursiveTask<double[]> right = new AssignValuesTask(list, mid, high);
 
-                // Obtain the second half
-                int secondHalfLength = list.length - list.length/2;
-                double[] secondHalf = new double[secondHalfLength];
-                System.arraycopy(list, list.length / 2, secondHalf, 0, secondHalfLength);
+                // Returns the right side of the array
+                right.fork();
 
-                // Recursively sort the two halves
-                invokeAll(new AssignValuesTask(firstHalf), new AssignValuesTask(secondHalf));
+                // Returns the left side of the array
+                left.fork();
 
-                // Merge firstHalf with secondHalf into list
-                int current1 = 0; // Current index in list1
-                int current2 = 0; // Current index in list2
-                int current3 = 0; // Current index in temp
-
-                while (current1 < firstHalf.length && current2 < secondHalf.length) {
-                    if (firstHalf[current1] < secondHalf[current2])
-                        list[current3++] = firstHalf[current1++];
-                    else
-                        list[current3++] = secondHalf[current2++];
-                }
-
-                while (current1 < firstHalf.length)
-                    list[current3++] = firstHalf[current1++];
-
-                while (current2 < secondHalf.length)
-                    list[current3++] = secondHalf[current2++];
+                return list;
             }
         }
     }
